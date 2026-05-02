34ba4b3ab1521ea5820b7c35a0e1f27590cba165
Shoyouuu <boykasbi2@gmail.com>
2026-05-02 07:23:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,7 @@
 
 set -e
 
-REPO="https://github.com/Shoyouuu/Ghuroba.git"
-FOLDER="Ghuroba"
 INSTALL_DIR="$PREFIX/bin"
-
-echo "[*] Checking dependencies..."
-
-if ! command -v git >/dev/null 2>&1; then
-    echo "[*] Installing git..."
-    pkg update -y && pkg install git -y || apt update && apt install git -y
-fi
-
-echo "[*] Cloning repository..."
-rm -rf "$FOLDER"
-git clone --depth=1 "$REPO"
-
-cd "$FOLDER"
 
 echo "[*] Setting permissions..."
 chmod +x ghurob
@@ -27,14 +12,10 @@ mkdir -p "$INSTALL_DIR"
 cp ghurob "$INSTALL_DIR/ghurob"
 chmod +x "$INSTALL_DIR/ghurob"
 
-echo "[*] Cleaning workspace..."
+echo "[*] Cleaning repo..."
 cd ..
-rm -rf "$FOLDER"
-
-echo "[*] Cleaning installer..."
-SCRIPT_PATH="$(realpath "$0")"
-rm -f "$SCRIPT_PATH"
+rm -rf Ghuroba
 
 echo "[✓] MAKASIH SUDAH BERSABAR🫣"
 echo "[*] Run with:"
-echo "    ghurob"
+echo "[*] ghurob"
