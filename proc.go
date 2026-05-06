c91a4cd38c515e57cba640e24584b37ad4c63c95
Shoyouuu <boykasbi2@gmail.com>
2026-05-07 03:33:25 +0700

    Update proc.go

--- proc.go
+++ proc.go
@@ -7,24 +7,24 @@ import (
 	"time"
 )
 
-// === SPAWN ANAK YATIM (Persistent Mode) ===
+
 func spawnChild() {
 	cmd := exec.Command(os.Args[0])
 	
-	// Kasih tanda anti-meledak
+	
 	cmd.Env = append(os.Environ(), "I_AM_CHILD=true")
 	
-	// INI KUNCINYA: Bikin grup proses sendiri supaya gak ikut mati pas Bapak di-CTRL+C
+	
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true, // Anak punya grup sendiri
+		Setpgid: true, 
 	}
 	
 	cmd.Start() 
-	// Tanpa cmd.Wait(), Bapak langsung lepas tangan (Anak jadi Yatim)
+	
 }
 
 func StartMultiProcess(jumlah int) {
-	// Anti Fork-Bomb
+	
 	if os.Getenv("I_AM_CHILD") == "true" {
 		return
 	}
